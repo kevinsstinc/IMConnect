d4839a0a1c78a84c49c0648be31774fd1823d2c1
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2025-05-29 12:38:15 +0800

    Initial Commit

---
+++ IMDAConnect/IMDAConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDAConnectApp.swift
+//  IMDAConnect
+//
+//  Created by Joseph Kevin Fredric on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct IMDAConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
