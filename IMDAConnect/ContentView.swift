a84f11c2aa8edbe950ca5e8117aad9c7fee805ba
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2025-05-29 12:45:57 +0800

    First Push

--- IMDAConnect/ContentView.swift
+++ IMDAConnect/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("First Push")
+            Text("First Push!")
         }
         .padding()
     }
